fd73e3e2b153f6a26e54bb3aa48ab9906b9f8ec6
Janghyub Seo <jhyub06@gmail.com>
2025-08-21 15:03:07 +0900

    use null-terminated strings in crypttab parse result

--- src/crypttab.zig
+++ src/crypttab.zig
@@ -1,19 +1,19 @@
 const std = @import("std");
 
 pub const CrypttabEntry = struct {
-    volumeName: []const u8,
-    encryptedDevice: []const u8,
-    keyFile: ?[]const u8,
-    options: ?[]const u8,
+    volumeName: [:0]const u8,
+    encryptedDevice: [:0]const u8,
+    keyFile: ?[:0]const u8,
+    options: ?[:0]const u8,
 };
 
-fn parseCrypttabEntry(line: []const u8) !CrypttabEntry {
+fn parseCrypttabEntry(allocator: std.mem.Allocator, line: []const u8) !CrypttabEntry {
     var parts = std.mem.splitSequence(u8, line, " ");
     return CrypttabEntry{
-        .volumeName = parts.next().?,
-        .encryptedDevice = parts.next().?,
-        .keyFile = parts.next(),
-        .options = parts.next(),
+        .volumeName = try allocator.dupeZ(u8, parts.next().?),
+        .encryptedDevice = try allocator.dupeZ(u8, parts.next().?),
+        .keyFile = if (parts.next()) |p| try allocator.dupeZ(u8, p) else null,
+        .options = if (parts.next()) |p| try allocator.dupeZ(u8, p) else null,
     };
 }
 
@@ -24,9 +24,22 @@ pub fn parseCrypttab(allocator: std.mem.Allocator, crypttab: []const u8) ![]Cryp
         if (line.len == 0) continue;
         if (line[0] == '#') continue;
         if (line[0] == '\n') continue;
-        const entry = try parseCrypttabEntry(line);
+        const entry = try parseCrypttabEntry(allocator, line);
         std.debug.print("{s} {s} {?s} {?s}\n", .{ entry.volumeName, entry.encryptedDevice, entry.keyFile, entry.options });
         try list.append(entry);
     }
     return list.toOwnedSlice();
+}
+
+pub fn freeCrypttab(allocator: std.mem.Allocator, entries: []CrypttabEntry) void {
+    for (entries) |entry| {
+        allocator.free(entry.volumeName);
+        allocator.free(entry.encryptedDevice);
+        if (entry.keyFile) |keyFile| {
+            allocator.free(keyFile);
+        }
+        if (entry.options) |options| {
+            allocator.free(options);
+        }
+    }
 }
