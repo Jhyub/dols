796bbbc8191b85222b080352aaf3f757d917ca80
Janghyub Seo <jhyub06@gmail.com>
2025-09-24 12:09:59 +0900

    add mkinitcpio-nfs-utils as dependency & clean build.zig

--- build.zig
+++ build.zig
@@ -4,11 +4,27 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Dependencies
+
+    const mkinitcpio_nfs_utils = b.dependency("mkinitcpio-nfs-utils", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const toml = b.dependency("toml", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Modules
+
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    lib_mod.addIncludePath(mkinitcpio_nfs_utils.path("ipconfig"));
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
@@ -17,6 +33,9 @@ pub fn build(b: *std.Build) void {
     });
 
     exe_mod.addImport("dols_lib", lib_mod);
+    exe_mod.addImport("toml", toml.module("toml"));
+
+    // libdols
 
     const lib = b.addLibrary(.{
         .linkage = .static,
@@ -29,20 +48,26 @@ pub fn build(b: *std.Build) void {
     lib.linkSystemLibrary("cryptsetup");
     lib.linkSystemLibrary("ssh");
 
+    lib.addCSourceFiles(.{
+        .root = mkinitcpio_nfs_utils.path("ipconfig"),
+        .files = &.{ "bootp_proto.c", "dhcp_proto.c", "netdev.c", "main.c", "packet.c" },
+    });
+    lib.installHeadersDirectory(mkinitcpio_nfs_utils.path("ipconfig"), "", .{
+        .include_extensions = &.{"ipconfig.h"},
+    });
+
     b.installArtifact(lib);
+
+    // dols
 
     const exe = b.addExecutable(.{
         .name = "dols",
         .root_module = exe_mod,
     });
 
-    const toml = b.dependency("toml", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("toml", toml.module("toml"));
-
     b.installArtifact(exe);
+
+    // run
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -53,6 +78,8 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // tests
 
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
