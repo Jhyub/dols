d116d9ad702eee613fbdcf6bf179a72bfa13225f
Janghyub Seo <jhyub06@gmail.com>
2025-09-27 02:00:14 +0900

    introduce configuration for ipconfig

--- src/config.zig
+++ src/config.zig
@@ -1,16 +1,26 @@
+const std = @import("std");
+
+pub const IpConfigConfig = struct {
+    interfaces: []const []const u8,
+    timeout: ?u32,
+};
+
 pub const ConfigFile = struct {
     port: ?u16,
     auth_try_limit: ?u32,
+    ip: ?IpConfigConfig,
 };
 
 pub const Config = struct {
     port: u16,
     auth_try_limit: u32,
+    ip: ?IpConfigConfig,
 
     pub fn init(from: ConfigFile) Config {
         return Config{
             .port = from.port orelse 22,
             .auth_try_limit = from.auth_try_limit orelse 0,
+            .ip = from.ip,
         };
     }
 };
