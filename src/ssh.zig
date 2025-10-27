5f746ddbf95db25ac0303bc60bcb5e2116d283b6
Janghyub Seo <jhyub06@gmail.com>
2025-10-27 11:41:51 +0900

    refactor `ssh.zig`

--- src/ssh.zig
+++ src/ssh.zig
@@ -62,44 +62,48 @@ pub fn startSshd(allocator: std.mem.Allocator, conf: *const config.Config, entri
 
         std.debug.print("All devices decrypted successfully\n", .{});
 
-        const ask_files = try sap.Ask.list(allocator);
-        defer allocator.free(ask_files);
-
-        if (ask_files.len > 0) {
-            var dir = try std.fs.cwd().openDir("/run/systemd/ask-password", .{ .iterate = true });
-            defer dir.close();
-            for (ask_files) |filename| {
-                defer allocator.free(filename);
-
-                const ask = try sap.Ask.readFrom(allocator, dir.openFile(filename, .{}) catch continue);
-                defer ask.deinit();
-
-                const proc_path = try std.fmt.allocPrint(allocator, "/proc/{d}", .{ask.pid});
-                defer allocator.free(proc_path);
-
-                var proc_dir = try std.fs.cwd().openDir(proc_path, .{ .iterate = true });
-                defer proc_dir.close();
-
-                var buf: [4096]u8 = undefined;
-                const exe = try proc_dir.readLink("exe", &buf);
-                if (std.mem.eql(u8, exe, "/usr/bin/systemd-cryptsetup")) {
-                    try ask.answer(allocator, "", true, false);
-                    break;
-                }
-            }
-        }
+        try finishSystemdAskPassword(allocator);
 
         return;
+    }
+}
+
+fn finishSystemdAskPassword(allocator: std.mem.Allocator) !void {
+    const ask_files = try sap.Ask.list(allocator);
+    defer allocator.free(ask_files);
+
+    if (ask_files.len > 0) {
+        var dir = try std.fs.cwd().openDir("/run/systemd/ask-password", .{ .iterate = true });
+        defer dir.close();
+        for (ask_files) |filename| {
+            defer allocator.free(filename);
+
+            const ask = try sap.Ask.readFrom(allocator, try dir.openFile(filename, .{}));
+            defer ask.deinit();
+
+            const proc_path = try std.fmt.allocPrint(allocator, "/proc/{d}", .{ask.pid});
+            defer allocator.free(proc_path);
+
+            var proc_dir = try std.fs.cwd().openDir(proc_path, .{ .iterate = true });
+            defer proc_dir.close();
+
+            var buf: [4096]u8 = undefined;
+            const exe = try proc_dir.readLink("exe", &buf);
+            if (std.mem.eql(u8, exe, "/usr/bin/systemd-cryptsetup")) {
+                try ask.answer(allocator, "", true, false);
+                break;
+            }
+        }
     }
 }
 
 fn authenticateUser(allocator: std.mem.Allocator, session: *c.ssh_session_struct) !bool {
     var msg: c.ssh_message = undefined;
 
-    const name = "Welcome to dols, v0.0.0-250913\n";
-    const instructionFormat = "Enter ssh password for user {s}";
-    var prompt: [1][*c]const u8 = .{@ptrCast("Password: ")};
-    var echo = [1]u8{0};
+    const kbdintName = "Welcome to dols, v0.0.0\n";
+    const kbdintInstructionFormat = "Enter ssh password for user {s}";
+    var kbdintPrompt: [1][*c]const u8 = .{@ptrCast("Password: ")};
+    var kbdintEcho = [1]u8{0};
 
     var username: [:0]const u8 = try allocator.dupeZ(u8, "");
     defer allocator.free(username);
@@ -108,110 +112,85 @@ fn authenticateUser(allocator: std.mem.Allocator, session: *c.ssh_session_struct
         msg = c.ssh_message_get(session);
         defer c.ssh_message_free(msg);
 
-        switch (c.ssh_message_type(msg)) {
-            c.SSH_REQUEST_AUTH => {
-                switch (c.ssh_message_subtype(msg)) {
-                    c.SSH_AUTH_METHOD_PASSWORD => {
-                        allocator.free(username);
-                        username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
-                        const password = std.mem.span(c.ssh_message_auth_password(msg));
-                        if (shadow.authenticateByUsername(username, password)) {
-                            _ = c.ssh_message_auth_reply_success(msg, 1);
-                            return true;
-                        }
-                        return false;
-                    },
-                    c.SSH_AUTH_METHOD_INTERACTIVE => {
-                        if (c.ssh_message_auth_kbdint_is_response(msg) == 0) {
-                            allocator.free(username);
-                            username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
-                            const instruction = try std.fmt.allocPrint(allocator, instructionFormat, .{username});
-                            defer allocator.free(instruction);
-                            _ = c.ssh_message_auth_interactive_request(msg, name, @ptrCast(instruction), 1, @ptrCast(&prompt), @ptrCast(&echo));
-                        } else {
-                            const reply = std.mem.span(c.ssh_userauth_kbdint_getanswer(session, 0));
-                            if (shadow.authenticateByUsername(username, reply)) {
-                                _ = c.ssh_message_auth_reply_success(msg, 1);
-                                return true;
-                            }
-                            return false;
-                        }
-                    },
-                    else => {
-                        allocator.free(username);
-                        username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
-                        _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
-                        _ = c.ssh_message_reply_default(msg);
-                    },
+        if(c.ssh_message_type(msg) != c.SSH_REQUEST_AUTH) {
+            _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
+            _ = c.ssh_message_reply_default(msg);
+            continue;
+        }
+
+        switch (c.ssh_message_subtype(msg)) {
+            c.SSH_AUTH_METHOD_PASSWORD => {
+                allocator.free(username);
+                username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
+                const password = std.mem.span(c.ssh_message_auth_password(msg));
+                if (shadow.authenticateByUsername(username, password)) {
+                    _ = c.ssh_message_auth_reply_success(msg, 1);
+                    return true;
+                }
+                return false;
+            },
+            c.SSH_AUTH_METHOD_INTERACTIVE => {
+                if (c.ssh_message_auth_kbdint_is_response(msg) == 0) {
+                    allocator.free(username);
+                    username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
+                    const kbtintInstruction = try std.fmt.allocPrint(allocator, kbdintInstructionFormat, .{username});
+                    defer allocator.free(kbtintInstruction);
+                    _ = c.ssh_message_auth_interactive_request(msg, kbdintName, @ptrCast(kbtintInstruction), 1, @ptrCast(&kbdintPrompt), @ptrCast(&kbdintEcho));
+                } else {
+                    if (c.ssh_userauth_kbdint_getnanswers(session) != 1) return false;
+                    const reply = std.mem.span(c.ssh_userauth_kbdint_getanswer(session, 0));
+                    if (shadow.authenticateByUsername(username, reply)) {
+                        _ = c.ssh_message_auth_reply_success(msg, 1);
+                        return true;
+                    }
+                    return false;
                 }
             },
             else => {
+                allocator.free(username);
+                username = try allocator.dupeZ(u8, std.mem.span(c.ssh_message_auth_user(msg)));
                 _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
                 _ = c.ssh_message_reply_default(msg);
             },
         }
     }
+
     return false;
-}
-
-fn kbdintCheckResponse(session: *c.ssh_session_struct, answers: []const []const u8) bool {
-    const count = c.ssh_userauth_kbdint_getnanswers(session);
-
-    if (count != answers.len) {
-        return false;
-    }
-
-    for (0..answers.len) |i| {
-        const reply = std.mem.span(c.ssh_userauth_kbdint_getanswer(session, @intCast(i)));
-        const answer = answers[i];
-        if (!std.mem.eql(u8, reply, answer)) {
-            return false;
-        }
-    }
-    return true;
 }
 
 fn tryDecrypt(allocator: std.mem.Allocator, session: *c.ssh_session_struct, entry: *crypttab.CrypttabEntry) !bool {
     var msg: c.ssh_message = undefined;
 
-    const name = "Welcome to dols, v0.0.0\n";
-    const instructionFormat = "Enter disk decryption password for {s}";
-    var prompt: [1][*c]const u8 = .{@ptrCast("Password: ")};
-    var echo = [1]u8{0};
+    const kbdintName = "Welcome to dols, v0.0.0\n";
+    const kbdintInstructionFormat = "Enter disk decryption password for {s}";
+    var kbdintPrompt: [1][*c]const u8 = .{@ptrCast("Password: ")};
+    var kbdintEcho = [1]u8{0};
 
     while (true) {
         msg = c.ssh_message_get(session);
         defer c.ssh_message_free(msg);
 
-        switch (c.ssh_message_type(msg)) {
-            c.SSH_REQUEST_AUTH => {
-                switch (c.ssh_message_subtype(msg)) {
-                    c.SSH_AUTH_METHOD_INTERACTIVE => {
-                        if (c.ssh_message_auth_kbdint_is_response(msg) == 0) {
-                            const instruction = try std.fmt.allocPrint(allocator, instructionFormat, .{entry.volumeName});
-                            defer allocator.free(instruction);
-                            _ = c.ssh_message_auth_interactive_request(msg, name, @ptrCast(instruction), 1, @ptrCast(&prompt), @ptrCast(&echo));
-                        } else {
-                            _ = cryptsetup.attachDeviceByPassword(entry, std.mem.span(c.ssh_userauth_kbdint_getanswer(session, 0))) catch {
-                                _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
-                                _ = c.ssh_message_reply_default(msg);
-                                return false;
-                            };
-                            _ = c.ssh_message_auth_reply_success(msg, 0);
-                            return true;
-                        }
-                    },
-                    else => {
-                        _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
-                        _ = c.ssh_message_reply_default(msg);
-                    },
-                }
-            },
-            else => {
+        if(c.ssh_message_type(msg) != c.SSH_REQUEST_AUTH or c.ssh_message_subtype(msg) != c.SSH_AUTH_METHOD_INTERACTIVE) {
+            _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
+            _ = c.ssh_message_reply_default(msg);
+            continue;
+        }
+
+        if (c.ssh_message_auth_kbdint_is_response(msg) == 0) {
+            const kbtintInstruction = try std.fmt.allocPrint(allocator, kbdintInstructionFormat, .{entry.volumeName});
+            defer allocator.free(kbtintInstruction);
+            _ = c.ssh_message_auth_interactive_request(msg, kbdintName, @ptrCast(kbtintInstruction), 1, @ptrCast(&kbdintPrompt), @ptrCast(&kbdintEcho));
+        } else {
+            if (c.ssh_userauth_kbdint_getnanswers(session) != 1) return false;
+            _ = cryptsetup.attachDeviceByPassword(entry, std.mem.span(c.ssh_userauth_kbdint_getanswer(session, 0))) catch {
                 _ = c.ssh_message_auth_set_methods(msg, c.SSH_AUTH_METHOD_INTERACTIVE);
                 _ = c.ssh_message_reply_default(msg);
-            },
+                return false;
+            };
+            _ = c.ssh_message_auth_reply_success(msg, 0);
+            return true;
         }
     }
+
     return false;
 }
